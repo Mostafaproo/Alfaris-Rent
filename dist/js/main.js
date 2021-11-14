9c77031143333a8164746b504954a6706cb582f2
mostafazidan <mostafa.livecode@gmail.com>
2021-11-14 13:06:38 +0200

    [Alfaris] first eddition

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+ 
+});
+
+// var swiper = new Swiper(".swiperHome", {
+//     // Optional parameters
+//     direction: "horizontal",
+//     loop: true,
+//     speed: 400,
+//     spaceBetween: 100,
+
+//     // If we need pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//       scrollbar: {
+//         el: '.swiper-scrollbar',
+//       },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+
+//     // And if we need scrollbar
+   
+//   });
