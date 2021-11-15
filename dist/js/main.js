518e6a8fc659c1e52c7645761e3d2a9c0ee9948b
mostafazidan <mostafa.livecode@gmail.com>
2021-11-15 22:23:31 +0200

    add main slider to home page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,18 @@
 $(document).ready(function () {
+  var swiper = new Swiper(".swiperHome", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    speed: 400,
+    spaceBetween: 0,
 
-
-
-    var swiper = new Swiper(".swiperHome", {
-        // Optional parameters
-        direction: "horizontal",
-        loop: true,
-        speed: 400,
-        spaceBetween: 0,
-      
-        // If we need pagination
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        scrollbar: {
-          el: ".swiper-scrollbar",
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      
-        // And if we need scrollbar
-      });
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 });
-
-
