e1063915d07158a7640708fa309d0028d2c88862
mostafazidan <mostafa.livecode@gmail.com>
2021-11-17 11:42:47 +0200

    add section welcome

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,26 +15,34 @@ $(document).ready(function () {
       el: ".swiper-scrollbar",
     },
   });
-});
 
+  var heights = [];
+
+  $(".elementor-element").each(function () {
+    heights.push($(this).height());
+  });
+
+  var maxHeight = Math.max.apply(null, heights);
+
+  $(".elementor-element").height(maxHeight);
+
+
+
+});
 
 
 // Get the navbar
 var navbar = document.getElementById("navigation");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop +50;
+var sticky = navbar.offsetTop + 50;
 
-window.onscroll = function(){
-  if(window.scrollY > sticky){
-    navbar.classList.add("sticky")
-    $('.navbar-brand').css('display','block')
-  }
-  else {
+window.onscroll = function () {
+  if (window.scrollY > sticky) {
+    navbar.classList.add("sticky");
+    $(".navbar-brand").css("display", "block");
+  } else {
     navbar.classList.remove("sticky");
-    $('.navbar-brand').css('display','none')
+    $(".navbar-brand").css("display", "none");
   }
-}
-
-
-
+};
